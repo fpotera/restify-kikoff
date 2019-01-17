3c19244eafba52a4e4b598ba69151758111ac421
Florin Potera <florin.potera@ic-consult.com>
2019-01-17 16:10:11 +0100

    swagger added

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 var restify = require('restify');
+const pathToSwaggerUi = require('swagger-ui-dist').absolutePath();
  
 const server = restify.createServer({
   name: 'myapp',
   version: '1.0.0'
 });
- 
+
+
+
 server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
- 
+
+server.get('/*', restify.plugins.serveStatic( {
+
+  directory: pathToSwaggerUi,
+
+  default: 'index.html'
+
+}));
+
 server.get('/echo/:name', function (req, res, next) {
   res.send(req.params);
   return next();
